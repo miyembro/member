03acc6b7c2999c43425948ded3ca4fe8c488cbac
Arjay Fuentes <arjay@gmail.com>
2025-03-18 15:30:30 +0100

    deleted build yaml

--- src/test/java/com/rjproj/memberapp/MemberApplicationTests.java
+++ src/test/java/com/rjproj/memberapp/MemberApplicationTests.java
@@ -3,7 +3,7 @@ package com.rjproj.memberapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.config.location=classpath:/application-prod.yml")
+@SpringBootTest(properties = "spring.cloud.config.enabled=false")
 class MemberApplicationTests {
 
 	@Test
