1898f0685423ac6492d981dbee662c282d99d29f
Arjay Fuentes <arjay@gmail.com>
2025-03-21 15:00:10 +0100

    change prod test

--- src/test/java/com/rjproj/memberapp/MemberApplicationTests.java
+++ src/test/java/com/rjproj/memberapp/MemberApplicationTests.java
@@ -3,7 +3,7 @@ package com.rjproj.memberapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.config.location=classpath:/application-test.yml")
+@SpringBootTest(properties = "spring.config.location=classpath:/application-prod-test.yml")
 class MemberApplicationTests {
 
 	@Test
